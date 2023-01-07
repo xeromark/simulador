63c20ec142faf05ae7c3b44027ae8fbe61bcbbe6
xeromark <122169269+xeromark@users.noreply.github.com>
2023-01-07 17:42:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.use(require('/src/routes/index'));
-
-app.use('/archives', express.static( 'archives'));      //esto agrega todo lo que es css y js
 
 
 app.listen(port);
