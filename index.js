63e524851f24f27073e241f3e58abb8e4b27fd9b
xeromark <122169269+xeromark@users.noreply.github.com>
2023-01-07 18:07:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
 
 const app = express();
 
-
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.use(require('./src/routes/index'));
 
