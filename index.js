a2baadffcb32824c38785bcf1481fb50b6815f93
xeromark <122169269+xeromark@users.noreply.github.com>
2023-01-07 17:48:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const express = require('express');
 const app = express();
 
 
-const port = process.env.PORT || 4000;
 
-app.listen(port);
 
 // Routes
 //app.use(require('/src/routes/index'));
@@ -16,10 +14,12 @@ app.listen(port);
 
 app.get('/' , (request, response) => {
 
-    response.sendFile(path.resolve(__dirname , "/index.html"))
+    response.sendFile(path.resolve(__dirname , "./index.html"))
 
 });
+const port = process.env.PORT || 4000;
 
+app.listen(port);
 
 
 console.log('Server on port', port);
